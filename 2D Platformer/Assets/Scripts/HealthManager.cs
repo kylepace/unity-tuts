789111cb78d53c598fd5e7a69ca616d67ceab757
kylepace <kylepace03@gmail.com>
2017-03-26 21:54:01 -0400

    2D beginning.

---
+++ 2D Platformer/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public GameObject[] hearts;
+	private int health;
+
+	// Use this for initialization
+	void Start () {
+		health = hearts.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Hazard") {
+			health--;
+			hearts[health].SetActive (false);
+		}
+
+		if (health == 0) {
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+	}
+}
