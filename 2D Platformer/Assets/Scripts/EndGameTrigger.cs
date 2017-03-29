224bb41625633ed53e3cc6376c48661ac564ad34
Kyle Pace <kylepace03@gmail.com>
2017-03-28 21:14:12 -0400

    Level2.

---
+++ 2D Platformer/Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EndGameTrigger : MonoBehaviour
+    {
+        public int levelnum = 0;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                Application.LoadLevel("Level" + levelnum);
+            }
+        }
+    }
+}
