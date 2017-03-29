75121baf93ab945a466add671bbbb0602da0fa69
Kyle Pace <kylepace03@gmail.com>
2017-03-28 20:47:30 -0400

    Level2.

--- 2D Platformer/Assets/Scripts/HealthManager.cs
+++ 2D Platformer/Assets/Scripts/HealthManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets._2D;
 
 public class HealthManager : MonoBehaviour {
 
 	public GameObject[] hearts;
-	private int health;
+	public Rigidbody2D playerBody;
+    public Platformer2DUserControl playerControl;
+
+    private int health;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +24,29 @@ public class HealthManager : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.tag == "Hazard") {
 			health--;
-			hearts[health].SetActive (false);
+			hearts[health].SetActive(false);
+            knockBack(coll.transform.position);
 		}
 
-		if (health == 0) {
+		if (health <= 0) {
 			Application.LoadLevel (Application.loadedLevelName);
 		}
 	}
+
+	void knockBack(Vector3 hazardPosition)
+	{
+	    StartCoroutine("haltMovement");
+	    var heading = transform.position - hazardPosition;
+	    var distance = heading.magnitude;
+	    var direction = heading / distance;
+        var directionForVelocity = new Vector2(direction.x, direction.y);
+	    playerBody.velocity = directionForVelocity * 20f;
+	}
+
+    IEnumerator haltMovement()
+    {
+        playerControl.movementEnabled = false;
+        yield return new WaitForSeconds(1);
+        playerControl.movementEnabled = true;
+    }
 }
